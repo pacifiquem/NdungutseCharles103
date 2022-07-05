ce3e2a5d66f004e3e7a535a8a8ebf62abc573e97
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-07-05 23:22:52 +0200

    new adjustments

--- src/components/TypefEffect.tsx
+++ src/components/TypefEffect.tsx
@@ -18,13 +18,13 @@ window.onload = function () {
     <h1 className='absolute tablet:mt-6 flex items-center bottom-0'>
       <p className=' tablet:text-3xl'>I&nbsp;</p>
       <p 
-        className="typewrite tablet:text-3xl"
+        className="typewrite tablet:text-2xl"
         data-period="1000"
         data-type='[" am software developer", " am Creative.", " Like to learn new things", " Love to Develop." ]'
       >
         <span className="wrap"></span>
       </p>
-      <p className='tablet:text-[2em] tablet:translate-y-[-3px] text-blue-900'>|</p>
+      <p className='tablet:text-[1.8em] tablet:translate-y-[-3px] text-blue-900'>|</p>
     </h1>
   );
 }
--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -6,21 +6,33 @@ import TypeEffect from "../components/TypefEffect";
 
 const Home = () => {
 	return (
-		<div className="flex relative home h-[70vh] tablet:h-[93vh] justify-center flex-col items-center w-full">
-			<div className="absolute flex top-0 right-0 w-full flex-col
-             items-center justify-center h-full bg-slate-200 bg-opacity-75">
-				<div className="relative h-[7vh] tablet:h-[10vh] flex flex-col ">
-                    <h2 className="tablet:text-3xl text-lg font-bold">
-				    	Hi. I'm Ishimwe Ndungutse Charles
-				    </h2>
-				    <TypeEffect />
-                </div>
-				<div className="flex mt-2 text-3xl text-blue-600 items-center">
-					<a className="https://www.linkedin.com/in/ishimwe-ndungutse-charles-079418227/" href=""><FaLinkedin /></a>
-					{/* <BiEnvelopeOpen /> */}
-					<a className="https://github.com/NdungutseCharles103" href=""><FaGithubSquare className="ml-4" /></a>
-					<a className="https://www.facebook.com/ishimwendungutsecharles" href=""><FaFacebookSquare className="ml-4" /></a>
-					<a className="https://www.instagram.com/ndungutse_charles/" href=""><FaInstagramSquare className="ml-4" /></a>
+		<div className="flex relative home h-[60vh] tablet:h-[93vh] justify-center flex-col items-center w-full">
+			<div className="absolute grid grid-cols-2 top-0 right-0 w-full
+            	 items-center justify-center h-full bg-slate-200 bg-opacity-100">
+				<div className="flex aspect-square items-center justify-center">
+					<img className="w-4/5" src="src/images/code.svg" alt="" />
+				</div>
+				<div className="flex items-center justify-center h-full flex-col aspect-square">
+					<div className="relative w-full h-[7vh] tablet:h-[10vh] flex flex-col ">
+            	        <h2 className="tablet:text-3xl text-lg font-bold">
+					    	Hi. I'm Ishimwe Ndungutse Charles
+					    </h2>
+					    <TypeEffect />
+            	    </div>
+					<div className="flex flex-col w-full items-start mt-2 text-blue-600">
+						<a className="flex items-center mt-5" href="https://www.linkedin.com/in/ishimwe-ndungutse-charles-079418227/"><FaLinkedin
+						className=" text-3xl" />
+						<p className="ml-4">Ishimwe Ndungutse Charles</p></a>
+						<a className="flex items-center mt-5" href="https://github.com/NdungutseCharles103" target="_blank"
+						 rel="no-referrer"><FaGithubSquare className=" text-3xl" />
+						 <p className="ml-4">Ndungutse Charles103</p></a>
+						<a className="flex items-center mt-5" href="https://www.facebook.com/ishimwendungutsecharles" target="_blank"
+						 rel="no-referrer"><FaFacebookSquare className=" text-3xl" />
+						 <p className="ml-4">Charles Hazard</p></a>
+						<a className="flex items-center mt-5" href="https://www.instagram.com/ndungutse_charles/" target="_blank"
+						 rel="no-referrer"><FaInstagramSquare className=" text-3xl" />
+						 <p className="ml-4">ndungutse_charles</p></a>
+					</div>
 				</div>
 			</div>
 		</div>
